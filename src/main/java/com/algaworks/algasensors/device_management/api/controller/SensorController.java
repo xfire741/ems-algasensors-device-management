de94870f663d9759ec1dce5b2a806ac2fb444495
Eduardo Melo <eduardovictor28@gmail.com>
2025-07-09 17:20:02 -0300

    10.7. Implementando paginação de resultados da consulta

--- src/main/java/com/algaworks/algasensors/device_management/api/controller/SensorController.java
+++ src/main/java/com/algaworks/algasensors/device_management/api/controller/SensorController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 
 import com.algaworks.algasensors.device_management.api.model.SensorInput;
@@ -26,6 +28,12 @@ import lombok.RequiredArgsConstructor;
 public class SensorController {
 
     private final SensorRepository sensorRepository;
+
+    @GetMapping
+    public Page<SensorOutput> search(Pageable pageable) {
+        Page<Sensor> sensors = sensorRepository.findAll(pageable);
+        return sensors.map(this::toOutput);
+    }
 
     @GetMapping("/{sensorId}")
     public SensorOutput getOne(@PathVariable TSID sensorId) {
